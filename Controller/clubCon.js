0793c076f8b0446db4d97bc7e63b29d913821d1e
Bennny20 <phientruong20@gmail.com>
2023-02-24 01:07:50 +0700

    update club API

--- Controller/clubCon.js
+++ Controller/clubCon.js
@@ -28,19 +28,30 @@ export const getClub = async (req, res, next) => {
   }
 };
 
-
-export const updateClub= async (req, res, next) => {
+export const updateClub = async (req, res, next) => {
   try {
+    const clubOid = await Club.findById(req.params.id);
+    console.log(clubOid);
+    const stadiumIdOld = clubOid.stadiumId;
     const stadiumId = req.body.stadiumId;
-    const updateClub= await Club.findByIdAndUpdate(
+
+    const updateClub = await Club.findByIdAndUpdate(
       req.params.id,
       { $set: req.body },
       { new: true }
     );
 
     try {
+      await Stadium.findByIdAndUpdate(stadiumIdOld, {
+        $pull: { Clubs: updateClub._id },
+      });
+    } catch (error) {
+      next(error);
+    }
+
+    try {
       await Stadium.findByIdAndUpdate(stadiumId, {
-        $push: { Clubs: saveClub._id },
+        $push: { Clubs: updateClub._id },
       });
     } catch (error) {
       next(error);
@@ -51,3 +62,29 @@ export const updateClub= async (req, res, next) => {
   }
 };
 
+export const deleteClub = async (req, res, next) => {
+  try {
+    const club = await Club.findById(req.params.id);
+    const stadiumId = club.stadiumId;
+    await Club.findByIdAndDelete(req.params.id);
+    try {
+      await Stadium.findByIdAndUpdate(stadiumId, {
+        $pull: { Clubs: club._id },
+      });
+    } catch (error) {
+      next(error);
+    }
+    res.status(200).json("Club has been delete");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllClub = async (req, res, next) => {
+  try {
+    const club = await Club.find();
+    res.status(200).json(club);
+  } catch (error) {
+    next(error);
+  }
+};
