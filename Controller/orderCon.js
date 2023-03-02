8499e3c46816bae914d76624e007f672d2939984
Bennny20 <phientruong20@gmail.com>
2023-03-02 23:05:27 +0700

    done order

--- Controller/orderCon.js
+++ Controller/orderCon.js
@@ -1,6 +1,7 @@
 import Order from "../model/Order.js";
 import OrderDetail from "../model/OrderDetail.js";
 import User from "../model/User.js";
+import TicketType from "../model/TicketType.js";
 
 export const createOrder = async (req, res, next) => {
   const newOrder = new Order(req.body);
@@ -11,7 +12,7 @@ export const createOrder = async (req, res, next) => {
   newOrder.date - Date.now();
 
   for (var x of newOrder.orderDeatails) {
-    const orderDetail = await OrderDetail.findById(x)
+    const orderDetail = await OrderDetail.findById(x);
     newOrder.totalPrice += orderDetail.total;
   }
   try {
@@ -22,6 +23,46 @@ export const createOrder = async (req, res, next) => {
   }
 };
 
+export const updateOrder = async (req, res, next) => {
+  try {
+    const updateOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).rjorn(updateOrder);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteOrder = async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  for (var x of order.orderDeatails) {
+    const orderDetail = await OrderDetail.findById(x);
+    const ticket = await TicketType.findById(orderDetail.ticketTypeId);
+    const refundTicket = ticket.quantity + orderDetail.amount;
+ 
+    await TicketType.findByIdAndUpdate(
+      orderDetail.ticketTypeId,
+      {
+        quantity: refundTicket,
+        status: true,
+      },
+      { new: true }
+    );
+
+    await OrderDetail.findByIdAndDelete(x);
+  }
+
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been delete.");
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getOrder = async (req, res, next) => {
   try {
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { vertifyAdmin } from "./../utils/vertifyToken.js";
-import { createOrder, getOrder, getById } from "../Controller/orderCon.js";
+import { createOrder, getOrder, getById, deleteOrder } from "../Controller/orderCon.js";
 
 const router = express.Router();
 
 router.post("/", vertifyAdmin, createOrder);
 router.get("/", getOrder);
 router.get("/:id", getById);
+router.delete("/:id", vertifyAdmin, deleteOrder);
 
 export default router;
