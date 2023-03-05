20facd32720234fb7a8e422e299ec5d89e8d442e
Bennny20 <phientruong20@gmail.com>
2023-03-06 00:42:14 +0700

    update

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { vertifyAdmin } from "./../utils/vertifyToken.js";
-import { createOrder, getOrder, getById, deleteOrder } from "../Controller/orderCon.js";
+import { createOrder, getOrder, getById, deleteOrder, getByUser } from "../Controller/orderCon.js";
 
 const router = express.Router();
 
 router.post("/", createOrder);
 router.get("/", getOrder);
 router.get("/:id", getById);
+router.get("/user/:id", getByUser);
 router.delete("/:id", deleteOrder);
 
 export default router;
